38479e4a34cfafedeb38376e615f69a27cbb27d7
Alfred Osorio <alfred.osorio@outlook.com>
2017-04-23 23:43:52 +1000

    add login form and authentication

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,5 +1,9 @@
 class UsersController < ApplicationController
 
+  def index
+    @users = User.all
+  end
+
   def new
     @user = User.new
   end
@@ -27,6 +31,11 @@ class UsersController < ApplicationController
       render 'edit'
     end
   end
+
+  def show
+    @user = User.find(params[:id])
+  end
+
   private
     def user_params
       params.require(:user).permit(:username, :email, :password)
